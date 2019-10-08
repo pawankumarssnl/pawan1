14d46014834bb7abc69168aee17791e5b669f6ef
pawankumarssnl <56216453+pawankumarssnl@users.noreply.github.com>
2019-10-08 12:06:03 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+'' '
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+' '' x = int (input ("enter any number")) if x > 2:for y in
+range (2, x)
+  :if (x % y)
+  == 0:
+    print (x, "is not a prime no.") break
+    else
+  :
+  print (x, "is a prime no.") break y += 1 elif x <= 1:
+   print (x, "is not a prime no.") elif x == 2:
+     print (x, "is a prime no.")
